9ce805a07f29797254aa1f9a32149c0a59cf1498
MaksGlushko <133649841+MaksGlushko@users.noreply.github.com>
2024-03-04 18:53:25 +0100

    123

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -4,12 +4,14 @@ const messageList = {
   403: "Forbidden",
   404: "Not Found",
   409: "Conflict",
-};
+}
 
-const HttpError = (status, message = messageList[status]) => {
-  const error = new Error(message);
-  error.status = status;
-  return error;
-};
+export class HttpError extends Error {
+  constructor(status, message = messageList[status]) {
+    super(message);
+    this.name = 'HttpError';
+    this.status = status;
+  }
+}
 
-module.exports = { HttpError };
+export default HttpError;
