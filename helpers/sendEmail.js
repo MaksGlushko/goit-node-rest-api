c0172d44e49489abadabe08f8c081b7ac69aa801
MaksGlushko <133649841+MaksGlushko@users.noreply.github.com>
2024-03-16 16:35:04 +0100

    upd final?

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -8,13 +8,14 @@ const nodemailerConfig = {
     port: 465,
     secure: true,
     auth: {
-        user: "maksregister@gmail.com",
-        pass: META_PASSWORD,
+        user: "maksglushko1231@meta.ua",
+        pass: 'Qetuosfhk2007',
     }
 };
 const transport = nodemailer.createTransport(nodemailerConfig);
+
 const sendEmail = data => {
-    const email = { ...data, from: "maksregister@gmail.com" };
+    const email = { ...data, from: "maksglushko1231@meta.ua" };
     return transport.sendMail(email);
 }
 
